ad6da34c834158d024922f1fc7bca09afb2220a3
Ayush vishnoi <240638@kit.ac.in>
2026-08-23 21:40:23 +0530

    Time: 198 ms (30.77%) | Memory: 15 MB (30.77%) - LeetSync

---
+++ 2039-sum-game/sum-game.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def sumGame(self, num):
+        n = len(num)
+        half = n // 2
+
+        left_sum = 0
+        right_sum = 0
+        left_q = 0
+        right_q = 0
+
+        for i in range(half):
+            if num[i] == '?':
+                left_q += 1
+            else:
+                left_sum += int(num[i])
+
+        for i in range(half, n):
+            if num[i] == '?':
+                right_q += 1
+            else:
+                right_sum += int(num[i])
+
+        # If total '?' is odd, Alice wins
+        if (left_q + right_q) % 2 == 1:
+            return True
+
+        # Check whether Bob can make both halves equal
+        return left_sum - right_sum != (right_q - left_q) * 9 // 2
